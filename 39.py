dd5cb34c359bc5ba650a774dfb31a4d24bdcb4f1
merdey <michaelerdey@gmail.com>
2016-01-09 14:42:54 -0800

    Improved 39

--- 39.py
+++ 39.py
@@ -10,20 +10,19 @@ For which value of p <= 1000, is the number of solutions maximised?
 from collections import defaultdict
 p_possibilities = defaultdict(set)
 
-print 'a'
+
+import math
 for a in range(2, 500):
     for b in range(1, a):
-        for c in range(3, 1001 - (a + b)):
-            if ((a ** 2) + (b ** 2)) == (c ** 2):
-                p = a + b + c
-                p_possibilities[p].add(frozenset([a, b, c]))
+        c = math.sqrt(a*a + b*b)
+        if c % 1 == 0:
+            p = a + b + c
+            p_possibilities[p].add(frozenset([a, b, c]))
 
 
-print 'b'
 max_p = 0
 max_poss = 0
 for p, poss in p_possibilities.items():
-    print p, poss, len(poss)
     if len(poss) > max_poss:
         max_p = p
         max_poss = len(poss)
